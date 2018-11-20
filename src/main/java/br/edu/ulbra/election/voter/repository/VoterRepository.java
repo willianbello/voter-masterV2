ea0f44dd2f985260959bab066f929cbf3c94fe62
willianbello <willianbello.s@gmail.com>
2018-11-20 00:01:26 -0200

    v.0.2.0

---
+++ src/main/java/br/edu/ulbra/election/voter/repository/VoterRepository.java
@@ -0,0 +1,10 @@
+package br.edu.ulbra.election.voter.repository;
+
+import br.edu.ulbra.election.voter.model.Voter;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VoterRepository extends CrudRepository<Voter, Long> {
+
+    Voter findFirstByEmail(String email);
+
+}
